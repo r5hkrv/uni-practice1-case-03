61dbe2405266ad03b43e4fd0306ae665ccb87e39
r5hkrv <202286897+r5hkrv@users.noreply.github.com>
2025-12-23 23:00:19 +0400

    (client) add notched text field component

---
+++ client/src/components/TextField.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { useTemplateRef } from "vue";
+
+defineProps<{ id: string; label: string; placeholder?: string }>();
+
+const notch = useTemplateRef("notch");
+
+const setFocused = (value: boolean) => {
+  if (notch.value === null) return;
+
+  if (value) {
+    notch.value.classList.add("is-focused");
+  } else {
+    notch.value.classList.remove("is-focused");
+  }
+};
+</script>
+
+<template>
+  <div class="field">
+    <label :for="`field-id-${id}`" class="field-label">
+      {{ label }}
+    </label>
+    <input
+      :id="`field-id-${id}`"
+      class="field-input"
+      type="text"
+      :placeholder="placeholder"
+      @focusin="setFocused(true)"
+      @focusout="setFocused(false)"
+    />
+    <fieldset ref="notch" class="field-notch" aria-hidden="true">
+      <legend class="field-notch-spacer">
+        {{ label }}
+      </legend>
+    </fieldset>
+  </div>
+</template>
+
+<style>
+.field {
+  --border-width: 2px;
+  --font-size: 0.75rem;
+  --field-focused-accent: oklch(0.72 0.24 235);
+  --notch-offset: 0.5rem;
+  --notch-spacer-padding: 0.25rem;
+  --notch-width: calc(var(--notch-offset) + var(--notch-spacer-padding));
+
+  position: relative;
+}
+
+.field-label {
+  padding-left: calc(var(--border-width) + var(--notch-width));
+
+  position: absolute;
+
+  font-size: var(--font-size);
+  font-weight: 700;
+
+  display: inline-block;
+}
+
+.field-notch {
+  padding-block: 0px;
+  padding-inline: var(--notch-offset);
+  margin: 0px;
+
+  position: absolute;
+  inset: 0px;
+
+  border: var(--border-width) solid oklch(0.88 0 0);
+  border-radius: 0.5rem;
+
+  pointer-events: none;
+
+  display: block;
+}
+
+.field-notch.is-focused {
+  border-color: var(--field-focused-accent);
+}
+
+.field-notch-spacer {
+  padding-inline: var(--notch-spacer-padding);
+
+  font-size: var(--font-size);
+
+  visibility: hidden;
+}
+
+.field-input {
+  width: 100%;
+  padding: 1rem;
+  border: none;
+  margin-top: calc(var(--font-size) / 2);
+
+  background-color: transparent;
+
+  font: inherit;
+
+  box-sizing: border-box;
+}
+
+.field-input:focus-visible {
+  outline: none;
+}
+</style>
